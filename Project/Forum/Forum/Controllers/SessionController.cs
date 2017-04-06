8f17eaa1f88f6fd393b6333f07923945bd821acd
Great V <GreatV@email.ua>
2017-04-06 14:11:06 +0300

    [New] Visual Studio solution

---
+++ Project/Forum/Forum/Controllers/SessionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forum.Controllers
+{
+    public class SessionController : Controller
+    {
+        public ActionResult LogIn()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Register()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
